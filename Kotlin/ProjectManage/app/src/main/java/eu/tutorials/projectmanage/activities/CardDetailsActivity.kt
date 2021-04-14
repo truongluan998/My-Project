2ad118e3dd69884fed2a47361ad8522a02fcf0ec
Truong Hong Luan <truongluan998@gmail.com>
2021-04-14 21:03:32 +0700

    color_feature

--- Kotlin/ProjectManage/app/src/main/java/eu/tutorials/projectmanage/activities/CardDetailsActivity.kt
+++ Kotlin/ProjectManage/app/src/main/java/eu/tutorials/projectmanage/activities/CardDetailsActivity.kt
@@ -2,6 +2,7 @@ package eu.tutorials.projectmanage.activities
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -9,6 +10,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import com.google.firebase.storage.FirebaseStorage
 import eu.tutorials.projectmanage.R
+import eu.tutorials.projectmanage.dialog.LabelColorListDialog
 import eu.tutorials.projectmanage.firebase.FireStoreClass
 import eu.tutorials.projectmanage.models.Board
 import eu.tutorials.projectmanage.models.Card
@@ -22,6 +24,7 @@ class CardDetailsActivity : BaseActivity() {
     private lateinit var mBoardDetails: Board
     private var mTaskListPosition = -1
     private var mCardPosition = -1
+    private var mSelectedColor = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,12 +37,21 @@ class CardDetailsActivity : BaseActivity() {
 
         et_name_card_details.setSelection(et_name_card_details.text.toString().length)
 
+        mSelectedColor = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].labelColor
+        if (mSelectedColor.isNotEmpty()) {
+            setColor()
+        }
+
         btn_update_card_details.setOnClickListener {
             if (et_name_card_details.text.toString().isNotEmpty()) {
                 updateCardDetails()
             } else {
                 Toast.makeText(this@CardDetailsActivity, "Enter a card name", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        tv_select_label_color.setOnClickListener {
+            labelColorsListDialog()
         }
 
     }
@@ -70,6 +82,23 @@ class CardDetailsActivity : BaseActivity() {
         finish()
     }
 
+    private fun colorsList(): ArrayList<String> {
+        val colorsList: ArrayList<String> = ArrayList()
+        colorsList.add("#43C86F")
+        colorsList.add("#0C90F1")
+        colorsList.add("#F72400")
+        colorsList.add("#7A8089")
+        colorsList.add("#D57C1D")
+        colorsList.add("#770000")
+        colorsList.add("#0022F8")
+        return colorsList
+    }
+
+    private fun setColor() {
+        tv_select_label_color.text = ""
+        tv_select_label_color.setBackgroundColor(Color.parseColor(mSelectedColor))
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
             R.id.action_delete_card -> {
@@ -97,11 +126,13 @@ class CardDetailsActivity : BaseActivity() {
         val card = Card (
             et_name_card_details.text.toString(),
             mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].createdBy,
-            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo,
+            mSelectedColor
          )
 
         mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition] = card
 
+        showProgressDialog(resources.getString(R.string.please_wait))
         FireStoreClass().addUpdateTaskList(this@CardDetailsActivity, mBoardDetails)
     }
 
@@ -140,5 +171,22 @@ class CardDetailsActivity : BaseActivity() {
         val alertDialog: AlertDialog = builder.create()
         alertDialog.setCancelable(false)
         alertDialog.show()
+    }
+
+    private fun labelColorsListDialog() {
+        val colorsList: ArrayList<String> = colorsList()
+
+        val listDialog = object: LabelColorListDialog(
+            this,
+            colorsList,
+            resources.getString(R.string.str_select_label_color),
+            mSelectedColor) {
+            override fun onItemSelected(color: String) {
+                mSelectedColor = color
+                setColor()
+            }
+
+        }
+        listDialog.show()
     }
 }
